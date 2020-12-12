868f05b52583c3511fafc51291c67443f11e3d26
FlutLab.io <>
2020-12-12 19:02:54 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientwidget/GeneratedCLIENTWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeurwidget/GeneratedVENDEURWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedadminwidget/GeneratedADMINWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedcardvdextdwidget/GeneratedCARDVDEXTDWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientaddcatclavierwidget/GeneratedClientADDCATClavierWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclienttlbwidget/GeneratedClientTLBWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientwidget/GeneratedClientWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientsectionwidget/GeneratedCLIENTSECTIONWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclienttlbwidget1/GeneratedClientTLBWidget1.dart';
+import 'package:flutterapp/sutch_clientapp/generatedlivreursectionwidget/GeneratedLIVREURSECTIONWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeuraddcatclavierwidget/GeneratedVendeurADDCATClavierWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeuradhtlbwidget/GeneratedVendeurADHTLBWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeuradhwidget/GeneratedVendeurADHWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeuraddcatclavierwidget1/GeneratedVendeurADDCATClavierWidget1.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeurtlbwidget/GeneratedVendeurTLBWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeurwidget/GeneratedVendeurWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeursectionwidget/GeneratedVENDEURSECTIONWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedaccueiltlbwidget/GeneratedAccueilTLBWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedaccueilwidget/GeneratedAccueilWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedinscriptionwidget/GeneratedInscriptionWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedconnexionwidget/GeneratedConnexionWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedadminsectionwidget/GeneratedADMINSECTIONWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeurinscriptionwidget/GeneratedVENDEURInscriptionWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvendeurconnexionwidget/GeneratedVENDEURConnexionWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientacceuilwidget/GeneratedCLIENTAcceuilWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedbkgwidget439/GeneratedBKGWidget439.dart';
+import 'package:flutterapp/sutch_clientapp/generatedrecnonwidget/GeneratedRECNONWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatednonwidget/GeneratedNONWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedgroup2widget/GeneratedGroup2Widget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedgroup1widget/GeneratedGroup1Widget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedvouspartezdejawidget/GeneratedVOUSPARTEZDEJAWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedcatavanceewidget1/GeneratedCATAVANCEEWidget1.dart';
+import 'package:flutterapp/sutch_clientapp/generatedbtincatwidget1/GeneratedBTINCATWidget1.dart';
+import 'package:flutterapp/sutch_clientapp/generatedslidebarwidget/GeneratedSlideBarWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientinterfacewidget/GeneratedCLIENTInterfaceWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientacceuilwidget1/GeneratedCLIENTAcceuilWidget1.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientinscriptionwidget/GeneratedCLIENTInscriptionWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedclientconnexionwidget/GeneratedCLIENTConnexionWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedlivreurinscriptionwidget/GeneratedLIVREURInscriptionWidget.dart';
+import 'package:flutterapp/sutch_clientapp/generatedlivreurconnexionwidget/GeneratedLIVREURConnexionWidget.dart';
+
+void main() {
+  runApp(sutch_clientApp());
+}
+
+class sutch_clientApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedCLIENTWidget',
+      routes: {
+        '/GeneratedCLIENTWidget': (context) => GeneratedCLIENTWidget(),
+        '/GeneratedVENDEURWidget': (context) => GeneratedVENDEURWidget(),
+        '/GeneratedADMINWidget': (context) => GeneratedADMINWidget(),
+        '/GeneratedCARDVDEXTDWidget': (context) => GeneratedCARDVDEXTDWidget(),
+        '/GeneratedClientADDCATClavierWidget': (context) =>
+            GeneratedClientADDCATClavierWidget(),
+        '/GeneratedClientTLBWidget': (context) => GeneratedClientTLBWidget(),
+        '/GeneratedClientWidget': (context) => GeneratedClientWidget(),
+        '/GeneratedCLIENTSECTIONWidget': (context) =>
+            GeneratedCLIENTSECTIONWidget(),
+        '/GeneratedClientTLBWidget1': (context) => GeneratedClientTLBWidget1(),
+        '/GeneratedLIVREURSECTIONWidget': (context) =>
+            GeneratedLIVREURSECTIONWidget(),
+        '/GeneratedVendeurADDCATClavierWidget': (context) =>
+            GeneratedVendeurADDCATClavierWidget(),
+        '/GeneratedVendeurADHTLBWidget': (context) =>
+            GeneratedVendeurADHTLBWidget(),
+        '/GeneratedVendeurADHWidget': (context) => GeneratedVendeurADHWidget(),
+        '/GeneratedVendeurADDCATClavierWidget1': (context) =>
+            GeneratedVendeurADDCATClavierWidget1(),
+        '/GeneratedVendeurTLBWidget': (context) => GeneratedVendeurTLBWidget(),
+        '/GeneratedVendeurWidget': (context) => GeneratedVendeurWidget(),
+        '/GeneratedVENDEURSECTIONWidget': (context) =>
+            GeneratedVENDEURSECTIONWidget(),
+        '/GeneratedAccueilTLBWidget': (context) => GeneratedAccueilTLBWidget(),
+        '/GeneratedAccueilWidget': (context) => GeneratedAccueilWidget(),
+        '/GeneratedInscriptionWidget': (context) =>
+            GeneratedInscriptionWidget(),
+        '/GeneratedConnexionWidget': (context) => GeneratedConnexionWidget(),
+        '/GeneratedADMINSECTIONWidget': (context) =>
+            GeneratedADMINSECTIONWidget(),
+        '/GeneratedVENDEURInscriptionWidget': (context) =>
+            GeneratedVENDEURInscriptionWidget(),
+        '/GeneratedVENDEURConnexionWidget': (context) =>
+            GeneratedVENDEURConnexionWidget(),
+        '/GeneratedCLIENTAcceuilWidget': (context) =>
+            GeneratedCLIENTAcceuilWidget(),
+        '/GeneratedBKGWidget439': (context) => GeneratedBKGWidget439(),
+        '/GeneratedRECNONWidget': (context) => GeneratedRECNONWidget(),
+        '/GeneratedNONWidget': (context) => GeneratedNONWidget(),
+        '/GeneratedGroup2Widget': (context) => GeneratedGroup2Widget(),
+        '/GeneratedGroup1Widget': (context) => GeneratedGroup1Widget(),
+        '/GeneratedVOUSPARTEZDEJAWidget': (context) =>
+            GeneratedVOUSPARTEZDEJAWidget(),
+        '/GeneratedCATAVANCEEWidget1': (context) =>
+            GeneratedCATAVANCEEWidget1(),
+        '/GeneratedBTINCATWidget1': (context) => GeneratedBTINCATWidget1(),
+        '/GeneratedSlideBarWidget': (context) => GeneratedSlideBarWidget(),
+        '/GeneratedCLIENTInterfaceWidget': (context) =>
+            GeneratedCLIENTInterfaceWidget(),
+        '/GeneratedCLIENTAcceuilWidget1': (context) =>
+            GeneratedCLIENTAcceuilWidget1(),
+        '/GeneratedCLIENTInscriptionWidget': (context) =>
+            GeneratedCLIENTInscriptionWidget(),
+        '/GeneratedCLIENTConnexionWidget': (context) =>
+            GeneratedCLIENTConnexionWidget(),
+        '/GeneratedLIVREURInscriptionWidget': (context) =>
+            GeneratedLIVREURInscriptionWidget(),
+        '/GeneratedLIVREURConnexionWidget': (context) =>
+            GeneratedLIVREURConnexionWidget(),
+      },
+    );
+  }
+}
